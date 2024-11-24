6ee03e4d2710714ba670a05ee6ad2a54d753ae13
Bruno <eubruno.rpa@gmail.com>
2024-11-24 17:37:20 -0300

    Adicionando a função de adicionar carro

--- project-estacionamento/app.py
+++ project-estacionamento/app.py
@@ -6,3 +6,23 @@ app  = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+
+@app.route('/add', methods=['GET','POST'])
+def add_carro():
+    if request.method == 'POST':
+        placa = request.form.get('placa')
+        modelo = request.form.get('modelo')
+        vaga = request.form.get('vaga')
+
+        if not placa or not modelo or not vaga: #Verificar se os campos estão todos preenchidos
+            return "Todos os campos são obrigatórios!",400
+        
+        if Carro.query.filter_by(vaga=vaga).first(): #Verifica se a vaga esta ocupada
+            return "Erro: Essa vaga já está ocupada!",400
+        
+        carro = Carro(placa=placa,modelo=modelo,vaga=vaga) #Cria novo carro e salva no DB
+        db.session.add(carro)
+        db.session.commit()
+        return redirect('/')
+    
+    return render_template('add.html') #Renderizar formulario para o carro
