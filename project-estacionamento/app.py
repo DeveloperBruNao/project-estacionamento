3cce68ab9925444bf1c1fb9550b182d951267090
Bruno <eubruno.rpa@gmail.com>
2024-11-24 17:29:12 -0300

    Inicialização do app flask e do sqlachelmy

--- project-estacionamento/app.py
+++ project-estacionamento/app.py
@@ -1,3 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 from models import db,Carro
 from config import Config
+
+app  = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
